15fcd882ea7fb2b30a24706e507c37444a2e31eb
Johnny Santamaria <johnnysantamaria603@gmail.com>
2025-04-30 10:01:50 -0700

    Update person.cpp
    Fixed the implementation and constructors

--- source/person.cpp
+++ source/person.cpp
@@ -1,55 +1,48 @@
-// #include "person.h"
-// #include <sstream>
+#include "person.h"
 
-// // Default constructor
-// Person::Person() : rating(0.0), totalRatings(0) {}
+// Default constructor
+Person::Person() : id(""), name(""), phone(""), rating(0.0), totalRatings(0) {}
 
-// // Parameterized constructor
-// Person::Person(const std::string& id, const std::string& name, const std::string& phone)
-//     : id(id), name(name), phone(phone), rating(0.0), totalRatings(0) {}
+// Parameterized constructor
+Person::Person(const std::string& id, const std::string& name, const std::string& phone)
+    : id(id), name(name), phone(phone), rating(0.0), totalRatings(0) {}
 
-// // Getters and setters
-// std::string Person::getId() const {
-//     return id;
-// }
+// Getters and Setters
+std::string Person::getId() const {
+    return id;
+}
 
-// void Person::setId(const std::string& id) {
-//     this->id = id;
-// }
+void Person::setId(const std::string& id) {
+    this->id = id;
+}
 
-// std::string Person::getName() const {
-//     return name;
-// }
+std::string Person::getName() const {
+    return name;
+}
 
-// void Person::setName(const std::string& name) {
-//     this->name = name;
-// }
+void Person::setName(const std::string& name) {
+    this->name = name;
+}
 
-// std::string Person::getPhone() const {
-//     return phone;
-// }
+std::string Person::getPhone() const {
+    return phone;
+}
 
-// void Person::setPhone(const std::string& phone) {
-//     this->phone = phone;
-// }
+void Person::setPhone(const std::string& phone) {
+    this->phone = phone;
+}
 
-// double Person::getRating() const {
-//     if (totalRatings == 0) {
-//         return 0.0;
-//     }
-//     return rating / totalRatings;
-// }
+double Person::getRating() const {
+    return rating;
+}
 
-// void Person::addRating(int rating) {
-//     if (rating >= 1 && rating <= 5) {
-//         this->rating += rating;
-//         totalRatings++;
-//     }
-// }
+void Person::addRating(int newRating) {
+    if (newRating < 0 || newRating > 5) return; // Validate rating
+    rating = ((rating * totalRatings) + newRating) / (totalRatings + 1);
+    ++totalRatings;
+}
 
-// // For file I/O
-// std::string Person::toString() const {
-//     std::stringstream ss;
-//     ss << id << "," << name << "," << phone << "," << rating << "," << totalRatings;
-//     return ss.str();
-// }
+// toString method for file I/O
+std::string Person::toString() const {
+    return id + "," + name + "," + phone + "," + std::to_string(rating);
+}
