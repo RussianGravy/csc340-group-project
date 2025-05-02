3dbcb9251c2b556052cecd98ea5d6cf631d0224e
RussianGravy <valentinejones1215@gmail.com>
2025-05-02 09:04:56 -0700

    new class members

---
+++ include/assignment.h
@@ -0,0 +1,14 @@
+#include "location.h"
+#include "rider.h"
+
+class Assignment
+{
+private:
+    std::string assignmentID;
+    int timeToDestination;
+    Rider passenger;
+    Location &pickUp;
+    Location &dropOff;
+
+public:
+}
