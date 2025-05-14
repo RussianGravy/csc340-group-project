da7e319a98a9dffda0e40e53011401499f7e9ff7
RussianGravy <valentinejones1215@gmail.com>
2025-05-14 05:48:58 -0700

    merged aaron fixes

--- main.cpp
+++ main.cpp
@@ -7,39 +7,7 @@
 int main()
 {
     System *system = new System();
-<<<<<<< HEAD
-    // making locations
-    Location sfstate = Location(2, 4, "SF State");
-    Location embarcadaero = Location(1, 1, "Embarcadero");
-    Location baybridge = Location(1, 2, "Bay Bridge");
-    Location mcdonalds = Location(1, 1, "McDonalds");
-    Location home = Location(2, 2, "Home");
-    std::vector<Location> locations = {sfstate, embarcadaero, baybridge, mcdonalds, home};
-
-    // adding drivers
-    Driver driver1 = Driver("Driver_Bob", "8YHH264");
-    driver1.add_location(sfstate);
-    Driver driver2 = Driver("Driver_Alex", "123ABC!");
-    driver2.add_location(embarcadaero);
-    Driver driver3 = Driver("Driver_Alice", "155HJ66");
-    driver3.add_location(baybridge);
-
-    system->addDriver(driver1);
-    system->addDriver(driver2);
-    system->addDriver(driver3);
-    // adding a request
-
-    // Aaron Note -- When you allocate it memory leaks here because your allocating your rider to the heap
-    // Aaron Note -- You might want to do this a bit differently because this also causes a memory leak.
-    /* Rider *testRider = new Rider("C1238895", "Rider_James", mcdonalds); */
-    /* system->addRequest(new Request(testRider, mcdonalds, home)); */
-
-    // Driver test_driver = read<Driver>("");
-    // cout << test_driver << '\n';
     // starting the program
-=======
-   	// starting the program
->>>>>>> 8dfb7efcad00548622fcef486aa9ed8c532d0edb
     system->start();
     // end the program and clean up memory
     delete system;
