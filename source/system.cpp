f4f20a3d2919aff48e34dca4c9a47527b96ebac5
RussianGravy <valentinejones1215@gmail.com>
2025-05-16 21:04:30 -0700

    another small update to menu

--- source/system.cpp
+++ source/system.cpp
@@ -213,7 +213,7 @@ void System::start()
             } 
             else
             {
-                cout << "You are number still in queue.\n";
+                cout << "You are still in queue.\n";
             }
         }
         else if (x == 7)
